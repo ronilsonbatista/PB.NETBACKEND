e82d62844131b5c0f43eb27020cec00cd624754f
Ronilson <ronilsondaconceicao@gmail.com>
2024-09-21 15:27:35 -0300

    tp4 save

--- Aluno.cs
+++ Aluno.cs
@@ -2,7 +2,6 @@ public class Aluno
 {
     public int Matricula { get; set; }
     public string Nome { get; set; }
-
     public Aluno(int matricula, string nome)
     {
         Matricula = matricula;
